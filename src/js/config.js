e4eafb3d964291079dad7a78151c4ee27dcaf02e
ade-ife <jadeniji54@gmail.com>
2022-02-02 15:58:17 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '10fc7851-d4c3-4dfc-9cad-558ed3c0eb7f';
+export const MODAL_CLOSE_SEC = 2.5;
